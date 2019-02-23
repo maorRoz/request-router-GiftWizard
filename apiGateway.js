d9815223513109ee478d00d5a13c7db40f3e3faf
maorroz <andory@post.bgu.ac.il>
2019-02-23 21:01:01 +0200

    added better systemTests

--- apiGateway.js
+++ apiGateway.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const express = require('express');
-const { requestRouter } = require('./index');
+const ExecutionService = require('./models/ExecutionService');
+const { requestRouter, addService } = require('./index');
 
 const app = express();
 
@@ -9,6 +10,11 @@ app.use('/', requestRouter);
 app.get('/*', (req, res) => {
   res.sendStatus(200);
 });
+
+const service1 = new ExecutionService(storeId => console.log(`yay1 ${storeId}`));
+const service2 = new ExecutionService(storeId => console.log(`yay2 ${storeId}`));
+addService(service1);
+addService(service2);
 
 const port = process.env.PORT || 3000;
 
