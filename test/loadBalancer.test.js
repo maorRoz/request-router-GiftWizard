5c12698056f470bacc085acce01fab4d14f05629
maorroz <andory@post.bgu.ac.il>
2019-02-23 15:04:21 +0200

    added load balancer tests

--- test/loadBalancer.test.js
+++ test/loadBalancer.test.js
@@ -10,29 +10,34 @@ const cleanService = (service = { stores: [] }) => {
 
 const validateService = (service, executeMethod, stores, numberOfCalls) => {
   expect(service.stores).to.deep.equal(stores);
-  stores.forEach((store, index) => expect(executeMethod.withArgs(store).callCount).to.equal(numberOfCalls[index]));
+  return stores.length > 0
+    ? stores.forEach((store, index) => expect(executeMethod.withArgs(store).callCount).to.equal(numberOfCalls[index]))
+    : expect(executeMethod.notCalled).to.equal(true);
 };
 
 describe('Load Balancer Tests', () => {
   let service1;
   let service2;
   let service3;
+  let spy1;
+  let spy2;
+  let spy3;
   const storeId1 = '1';
   const storeId2 = '2';
+  const storeId3 = '3';
   describe('Only one subscriber', () => {
-    it('One Id has been called once', () => {
-      const spy1 = sinon.spy();
+    let spy1;
+    beforeEach(() => {
+      spy1 = sinon.spy();
       service1 = new ExecutionService(spy1);
       loadBalancer.subscribe(service1);
+    });
+    it('One Id has been called once', () => {
       loadBalancer.handle(storeId1);
       validateService(service1, spy1, [storeId1], [1]);
     });
 
     it('Two differnet Ids has been called once', () => {
-      const spy1 = sinon.spy();
-      service1 = new ExecutionService(spy1);
-      loadBalancer.subscribe(service1);
-
       loadBalancer.handle(storeId1);
       validateService(service1, spy1, [storeId1], [1]);
       loadBalancer.handle(storeId2);
@@ -40,13 +45,69 @@ describe('Load Balancer Tests', () => {
     });
 
     it('One Id has been called twice', () => {
-      const spy1 = sinon.spy();
-      service1 = new ExecutionService(spy1);
-      loadBalancer.subscribe(service1);
-
       loadBalancer.handle(storeId1);
       loadBalancer.handle(storeId1);
       validateService(service1, spy1, [storeId1], [2]);
+    });
+
+    it('Two differnet Ids has been called multiple times', () => {
+      loadBalancer.handle(storeId1);
+      loadBalancer.handle(storeId2);
+      loadBalancer.handle(storeId1);
+      loadBalancer.handle(storeId1);
+      loadBalancer.handle(storeId2);
+      validateService(service1, spy1, [storeId1, storeId2], [3, 2]);
+    });
+  });
+
+  describe('Many subscribers', () => {
+    beforeEach(() => {
+      spy1 = sinon.spy();
+      spy2 = sinon.spy();
+      spy3 = sinon.spy();
+      service1 = new ExecutionService(spy1);
+      service2 = new ExecutionService(spy2);
+      service3 = new ExecutionService(spy3);
+      loadBalancer.subscribe(service1);
+      loadBalancer.subscribe(service2);
+      loadBalancer.subscribe(service3);
+    });
+
+    it('One Id has been called once', () => {
+      loadBalancer.handle(storeId1);
+      validateService(service1, spy1, [storeId1], [1]);
+      validateService(service2, spy2, [], []);
+      validateService(service3, spy3, [], []);
+    });
+
+    it('One Id has been called multiple times', () => {
+      loadBalancer.handle(storeId1);
+      loadBalancer.handle(storeId1);
+      validateService(service1, spy1, [storeId1], [2]);
+      validateService(service2, spy2, [], []);
+      validateService(service3, spy3, [], []);
+    });
+
+    it('Many Ids has been called once', () => {
+      loadBalancer.handle(storeId1);
+      loadBalancer.handle(storeId2);
+      loadBalancer.handle(storeId3);
+      validateService(service1, spy1, [storeId1], [1]);
+      validateService(service2, spy2, [storeId2], [1]);
+      validateService(service3, spy3, [storeId3], [1]);
+    });
+
+    it('Many Ids has been called multiple times', () => {
+      loadBalancer.handle(storeId1);
+      loadBalancer.handle(storeId1);
+      loadBalancer.handle(storeId2);
+      loadBalancer.handle(storeId3);
+      loadBalancer.handle(storeId3);
+      loadBalancer.handle(storeId2);
+      loadBalancer.handle(storeId3);
+      validateService(service1, spy1, [storeId1], [2]);
+      validateService(service2, spy2, [storeId2], [2]);
+      validateService(service3, spy3, [storeId3], [3]);
     });
   });
 
